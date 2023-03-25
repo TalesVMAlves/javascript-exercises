9a026f00a1b00d15a45583b52cdcf47b56cbd62a
Tales <tales.v.m.alves@gmail.com>
2023-03-25 16:43:18 -0300

    Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...element) {
+    let new_arr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(!element.includes(arr[i])){
+            console.log('i: '+i);
+            new_arr.push(arr[i]);
+        }
+    }
+    return new_arr;
 };
 
 // Do not edit below this line
