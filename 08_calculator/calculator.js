1f639c5feb2bafca03e2d62d1c3e5432cf4f2853
Tales <tales.v.m.alves@gmail.com>
2023-03-25 17:52:43 -0300

    Passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1+n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1-n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let soma = 0;
+  for(let i = 0; i < array.length; i++)   soma += array[i];
+  return soma;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let prod = 1;
+  for(let i = 0; i < array.length; i++)   prod = prod * array[i];
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	let powered_n = 1;
+  for(let i = 0; i < n2; i++)             powered_n = powered_n * n1;
+  return powered_n;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let fact = 1;
+  for(let i = number; i > 0; i--)     fact = fact * i;
+  return fact;
 };
 
 // Do not edit below this line
