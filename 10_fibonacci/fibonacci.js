6c9cbfd56f3b162e4623324561595e7db0d133ea
Tales <tales.v.m.alves@gmail.com>
2023-03-25 20:44:23 -0300

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(position > 0){
+        let fib = [1, 1];
+        for(let i = 2; i < position; i++){
+            fib.push(fib[i - 1] + fib[i - 2]);
+        }
+        return fib[position - 1];
+    }
+    else    return 'OOPS';
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
