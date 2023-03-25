f22d0c7122c46429aa044d328aaf9c2a092ed0ad
Tales <tales.v.m.alves@gmail.com>
2023-03-25 20:43:54 -0300

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    no_punctuation = phrase.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toUpperCase();
+    for(let i = 0; i < (no_punctuation.length)/2; i++){
+        if(no_punctuation[i] !== no_punctuation[no_punctuation.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
