a3b4ee4ff2cfbf0611dd0d230b4a089fd774f5da
Tales <tales.v.m.alves@gmail.com>
2023-03-24 21:41:17 -0300

    All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, n_repeats) {
+    if(n_repeats < 0)   return 'ERROR';
+    let blank = '';
+    for(let i = 0; i < n_repeats; i++){
+        blank += phrase;
+    }
+    return blank;
 };
 
 // Do not edit below this line
